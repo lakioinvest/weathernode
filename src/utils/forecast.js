5a6e017c81f43d2a323aac16ad69776bd21b8a91
Ulf Swedin <ulf@swedin.nu>
2025-08-07 14:27:23 +0200

    Updated forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,31 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=a826855aa90ccee2e93a093190b99c1d&query=' + latitude + ',' + longitude + '&units=m'
+  const url =
+    "http://api.weatherstack.com/current?access_key=a826855aa90ccee2e93a093190b99c1d&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=m";
 
-    request({ url, json: true }, (error, {body}) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees celsius outdoors.")
-        }
-    })
-}
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degrees celsius outdoors. It feels like " +
+          body.current.feelslike +
+          " degrees. Humidity is: " +
+          body.current.humidity
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
